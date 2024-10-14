d910cce43f71aa9cb2ef8195a277278e8213721a
YuboDi <yubodi@usc.edu>
2024-10-13 23:25:33 -0700

    Version Commit

--- Assets/Scripts/Player/MouseMovement.cs
+++ Assets/Scripts/Player/MouseMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 500f;
+    public float mouseSensitivity = 300f;
     public float topClamp = -90f;
     public float bottomClamp = 90f;
 
