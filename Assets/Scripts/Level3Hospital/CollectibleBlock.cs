69d7ff49874bcfb40bbcda11184529d8cfe92371
Enzo2599 <weinanz@usc.edu>
2024-11-07 20:26:12 -0800

    Puzzle but not ready

---
+++ Assets/Scripts/Level3Hospital/CollectibleBlock.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CollectibleBlock : MonoBehaviour
+{
+    public int prefabIndex; // 方块对应的 prefab 索引
+
+    private void Start()
+    {
+        // 设置方块名称与其对应的字母相同
+        switch (prefabIndex)
+        {
+            case 1:
+                gameObject.name = "G";
+                break;
+            case 2:
+                gameObject.name = "A";
+                break;
+            case 3:
+                gameObject.name = "T";
+                break;
+            case 4:
+                gameObject.name = "E";
+                break;
+            default:
+                break;
+        }
+    }
+}
