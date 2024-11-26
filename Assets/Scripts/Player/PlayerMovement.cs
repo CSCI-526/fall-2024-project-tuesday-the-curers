536e0484632bb32ca954f51e6e93b8af13617f03
Peng Han <penghan@usc.edu>
2024-11-26 01:49:11 -0800

    Gradually decelerate in wheatfield

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,106 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerMovement : MonoBehaviour
+//{
+//    public float speed = 12f;
+//    public float reducedSpeed = 6f; // hppp
+//    public float gravity = -9.81f;
+//    public float jumpHeight = 3f;
+//    private float originalSpeed; // hpppp
+
+//    public Transform groundCheck;
+//    public float groundDistance = 0.4f;
+//    public LayerMask groundMask;
+
+//    Vector3 velocity;
+
+//    bool isGrounded;
+//    bool isMoving;
+
+//    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+//    private CharacterController characterController;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        characterController = GetComponent<CharacterController>();
+//        originalSpeed = speed; // hppppp
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+//        if (isGrounded && velocity.y < 0)
+//        {
+//            velocity.y = -2f;
+//        }
+
+//        float x = Input.GetAxis("Horizontal");
+//        float z = Input.GetAxis("Vertical");
+
+//        Vector3 moveDirection = transform.right * x + transform.forward * z;
+
+//        characterController.Move(moveDirection * speed * Time.deltaTime);
+
+//        //if(Input.GetButton("Jump") && isGrounded)
+//        //{
+//        // velocity.y = Mathf.Sqrt(jumpHeight * -1f * gravity);
+//        //}
+
+//        velocity.y += gravity * Time.deltaTime;
+
+//        characterController.Move(velocity * Time.deltaTime);
+
+//        if (lastPosition != gameObject.transform.position && isGrounded == true)
+//        {
+//            isMoving = true;
+//        }
+//        else
+//        {
+//            isMoving = false;
+//        }
+
+//        lastPosition = gameObject.transform.position;
+//    }
+
+//    // hppppppppp
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("SlowZone")) // hpppppppp
+//        {
+//            speed = reducedSpeed; // hppppppp
+//        }
+//    }
+
+//    // hpppppp
+//    void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("SlowZone"))
+//        {
+//            speed = originalSpeed; // hppppppp
+//        }
+//    }
+
+
+//}
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 12f;
-    public float reducedSpeed = 6f; // hppp
+    public float speed = 12f;          // 正常速度
+    public float reducedSpeed = 2f;   // 最低速度
+    public float slowDownRate = 10f;   // 减速速率（每秒减少的速度值）
+    public float speedUpRate = 8f;    // 加速速率（每秒增加的速度值）
     public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-    private float originalSpeed; // hpppp
+
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -16,76 +108,66 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
 
-    bool isGrounded;
-    bool isMoving;
-
-    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+    private float currentSpeed;       // 当前速度
     private CharacterController characterController;
+    private HashSet<Collider> activeSlowZones = new HashSet<Collider>(); // 当前触发的稻草集合
 
-    // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        originalSpeed = speed; // hppppp
+        currentSpeed = speed; // 初始化速度为正常速度
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // 检测是否在地面上
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        // 获取输入
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
         Vector3 moveDirection = transform.right * x + transform.forward * z;
 
-        characterController.Move(moveDirection * speed * Time.deltaTime);
+        // 根据当前速度移动
+        characterController.Move(moveDirection * currentSpeed * Time.deltaTime);
 
-        //if(Input.GetButton("Jump") && isGrounded)
-        //{
-        // velocity.y = Mathf.Sqrt(jumpHeight * -1f * gravity);
-        //}
-
+        // 应用重力
         velocity.y += gravity * Time.deltaTime;
-
         characterController.Move(velocity * Time.deltaTime);
 
-        if (lastPosition != gameObject.transform.position && isGrounded == true)
+        // 动态调整速度
+        if (activeSlowZones.Count > 0 && currentSpeed > reducedSpeed)
         {
-            isMoving = true;
+            currentSpeed -= slowDownRate * Time.deltaTime; // 逐步减速
+            currentSpeed = Mathf.Max(currentSpeed, reducedSpeed); // 确保不会低于最低速度
         }
-        else
+        else if (activeSlowZones.Count == 0 && currentSpeed < speed)
         {
-            isMoving = false;
+            currentSpeed += speedUpRate * Time.deltaTime; // 逐步恢复速度
+            currentSpeed = Mathf.Min(currentSpeed, speed); // 确保不会超过正常速度
         }
-
-        lastPosition = gameObject.transform.position;
     }
 
-    // hppppppppp
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("SlowZone")) // hpppppppp
+        if (other.CompareTag("SlowZone"))
         {
-            speed = reducedSpeed; // hppppppp
+            activeSlowZones.Add(other); // 将触发器加入集合
+            Debug.Log($"Entered SlowZone: Active Zones = {activeSlowZones.Count}");
         }
     }
 
-    // hpppppp
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("SlowZone"))
         {
-            speed = originalSpeed; // hppppppp
+            activeSlowZones.Remove(other); // 从集合中移除触发器
+            Debug.Log($"Exited SlowZone: Active Zones = {activeSlowZones.Count}");
         }
     }
-
-
 }
-
-
